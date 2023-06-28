e54a75677f54b5d8447df6302a792ea1bb1eec68
duongtt1 <kitaro183@gmail.com>
2023-06-28 23:59:26 +0700

    add quetion

--- Apps/routes/QuetionRoute.js
+++ Apps/routes/QuetionRoute.js
@@ -9,11 +9,15 @@ const {
     getQuetion,
     deleteQuetion,
     updateQuetion,
-    getQuetionMdw
+    getQuetionMdw,
+    getQuetionCustomDA,
+    addQuetionCustomDA
 } = require("../controllers/QuetionController");
 
 router.route("/").post(createQuetion).get(getAllQuetions);
 router.get('/:id', getQuetionMdw, getQuetion)
+router.get('/custom/:id', getQuetionCustomDA)
+router.post('/custom/:id', addQuetionCustomDA)
 router.delete('/:id', getQuetionMdw, deleteQuetion)
 
 router.put('/:id', getQuetionMdw, updateQuetion);
