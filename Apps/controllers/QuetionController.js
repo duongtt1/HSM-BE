b183df9cf17b40f4e6d4cf2ffbb37b2afcb904c0
duongtt1 <kitaro183@gmail.com>
2023-06-30 21:35:03 +0700

    ok

--- Apps/controllers/QuetionController.js
+++ Apps/controllers/QuetionController.js
@@ -1,8 +1,30 @@
 const asyncHandler = require("../middlewares/async");
 const ErrorResponse = require("../utils/errorResponse");
-
+const fs = require('fs');
+const path = require('path');
 //! Models
 const QuetionModel = require("../models/QuetionModel");
+
+
+function readJSONFromFile(filePath) {
+    try {
+        const jsonData = fs.readFileSync(filePath, 'utf8');
+        return JSON.parse(jsonData);
+    } catch (error) {
+        console.error('Error reading JSON file:', error);
+        return null;
+    }
+}
+
+function writeJSONToFile(filePath, jsonData) {
+    try {
+        const jsonStr = JSON.stringify(jsonData, null, 2);
+        fs.writeFileSync(filePath, jsonStr, 'utf8');
+        console.log('Data written to JSON file successfully.');
+    } catch (error) {
+        console.error('Error writing JSON file:', error);
+    }
+}
 
 exports.createQuetion = asyncHandler(async (req, res, next) => {
     try {
@@ -63,109 +85,27 @@ exports.updateQuetion = asyncHandler(async (req, res, next) => {
 });
 
 exports.getQuetionCustomDA = asyncHandler(async (req, res, next) => {
-    data = {
-        "success": true,
-        "inputTimer": "10",
-        "data": [
-            {
-                "content": "Javascript là ngôn ngữ thông dịch hay biên dịch",
-                "options": [
-                    {
-                        "description": "A. Thông dịch",
-                    },
-                    {
-                        "description": "B. Biên dịch",
-                    },
-                    {
-                        "description": "C. Cả hai dạng",
-                    },
-                    {
-                        "description": "D. Không có dạng nào ở trên",
-                    },
-                ],
-                "answer": 3,
-            },
-            {
-                "content": "Javascript là ngôn ngữ kịch bản có dấu được mã nguồn không?",
-                "options": [
-                    {
-                        "description": "A. Không dấu được vì các kịch bản chạy ở client.",
-                    },
-                    {
-                        "description":
-                            "B. Dấu được vì chương trình hoạt động độc lập với trình duyệt",
-                    },
-                    {
-                        "description": "C. Hai phát biểu đều sai.",
-                    },
-                    {
-                        "description": "D. Hai phát biểu đều đúng",
-                    },
-                ],
-                "answer": 1,
-            },
-
-            {
-                "content": "JavaScript được bắt đầu bằng?",
-                "options": [
-                    {
-                        "description": "A. <scritp> …</script>",
-                    },
-                    {
-                        "description": "B. <Javascript> …<Javascript>",
-                    },
-                    {
-                        "description": "C. <java>  </java>",
-                    },
-                    {
-                        "description": "D. Tất cả các dạng trên.",
-                    },
-                ],
-                "answer": 4,
-            },
-            {
-                "content": "Lệnh prompt trong Javascript để làm gì?",
-                "options": [
-                    {
-                        "description": "A. Hiện một thông báo nhập thông tin",
-                    },
-                    {
-                        "description": "B. Hiện một thông báo dạng yes, No",
-                    },
-                    {
-                        "description": "C. Cả hai dạng trên",
-                    },
-                    {
-                        "description": "D. Không có lệnh nào đúng",
-                    },
-                ],
-                "answer": 1,
-            },
-            {
-                "content": "Lệnh lặp for có dạng như thế nào?",
-                "options": [
-                    {
-                        "description": "A. For ( biến = Giá trị đầu, Điều kiện, Giá trị tăng)",
-                    },
-                    {
-                        "description": "B. For ( biến = Giá trị đầu, Giá trị tăng, điều kiện)",
-                    },
-                    {
-                        "description": "C. For ( biến = Điều kiện, Giá trị tăng Giá trị cuối)",
-                    },
-                    {
-                        "description": "D. Tất cả các dạng trên.",
-                    },
-                ],
-                "answer": 1,
-            }
-        ]
-    }
-    res.status(200).json(data);
+    const relativePath = '../mocks/quetions.json';
+    const filePath = path.join(__dirname, relativePath);
+    data = readJSONFromFile(filePath);
+    console.log(data);
+    res.status(200).json(data.questions);
 });
 
 exports.addQuetionCustomDA = asyncHandler(async (req, res, next) => {
-    res.status(200).json({ success: true });
+    const relativePath = '../mocks/quetions.json';
+    const filePath = path.join(__dirname, relativePath);
+    data = readJSONFromFile(filePath);
+    let q = {};
+    q.answer = req.body.correctAnswer;
+    // q.inputTimer = req.body.inputTimer;
+    q.options = req.body.options;
+    q.content = req.body.tittle;
+    data.questions.data.push(q);
+    writeJSONToFile(filePath, data);
+    res.status(200).json({
+        "success" : true
+    });
 })
 
 // Middleware function to get a single user by ID
