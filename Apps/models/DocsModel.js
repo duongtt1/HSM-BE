be8aa59acc096b14365b73352dc3659ec6c74d4b
duongtt1 <kitaro183@gmail.com>
2023-06-30 20:47:14 +0700

    ignore required

--- Apps/models/DocsModel.js
+++ Apps/models/DocsModel.js
@@ -2,12 +2,12 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var DocsModel = new Schema({
-    nameDoc: { type: String, required: true, unique: true },
-    url: { type: String, required: true, unique: true },
-    type: { type: String, enum: ["PPTX", "PDF"], default: "PDF", required: true },
-	author: { 
-		type: Schema.Types.ObjectId, 
-		required: true, 
+	nameDoc: { type: String, required: true, unique: true },
+	url: { type: String, required: true, unique: true },
+	type: { type: String, enum: ["PPTX", "PDF"], default: "PDF", required: true },
+	author: {
+		type: Schema.Types.ObjectId,
+		// required: true, 
 		ref: "UserModel",
 		unique: true
 	},
