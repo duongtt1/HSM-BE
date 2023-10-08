331b87d3e2e7e46744f6efe48c5c9d0ab291c8eb
duongtt <kitaro183@gmail.com>
2023-10-08 13:37:32 +0700

    update

--- scripts/create_dotenv.sh
+++ scripts/create_dotenv.sh
@@ -6,10 +6,14 @@ if [ $# -ne 3 ]; then
     exit 1
 fi
 
-# Write to .env file
-echo "NODE_ENV=$1" > .env
-echo "MONGO_URI=$2" >> .env
-echo "PORT=$3" >> .env
+# # Write to .env file
+# echo "NODE_ENV=$1" > .env
+# echo "MONGO_URI=$2" >> .env
+# echo "PORT=$3" >> .env
+
+echo "NODE_ENV=$NODE_ENV" > .env
+echo "MONGO_URI=$MONGO_URI" >> .env
+echo "PORT=$PORT" >> .env
 
 echo "Parameters written to .env file:"
 # cat .env
